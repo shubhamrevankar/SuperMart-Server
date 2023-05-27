370db1b1edaca0c9248764daffac19ee40b19ac1
Shubham <shubhamsrevankar8668@gmail.com>
2023-05-27 23:43:03 +0530

    Errors in  new server

---
+++ app.js
@@ -0,0 +1,143 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import cors from "cors";
+
+import Stripe from "stripe";
+const stripe = Stripe(`${process.env.STRIPE_SECRET_KEY}`);
+
+const app = express();
+dotenv.config();
+
+connectDB();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/orders", orderRoutes);
+
+// const storeItems = new Map([
+//   [1, { priceInCents: 10000, name: "Learn React Today" }],
+//   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+// ]);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+// const calculateOrderAmount = (items) => {
+//   // Replace this constant with a calculation of the order's amount
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   return 1400;
+// };
+
+// app.post("/create-checkout-session", async (req, res) => {
+//   // console.log("inside checkout controller");
+//   try {
+//     console.log(req.body);
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment",
+//       line_items: req.body.items?.map((item) => {
+//         const storeItem = storeItems.get(item.id);
+//         return {
+//           price_data: {
+//             currency: "usd",
+//             product_data: {
+//               name: storeItem.name,
+//             },
+//             unit_amount: storeItem.priceInCents,
+//           },
+//           quantity: item.quantity,
+//         };
+//       }),
+//       success_url: `https://localhost:3000/success`,
+//       cancel_url: `https://localhost:3000/cancel`,
+//     });
+//     res.json({ url: session.url });
+//   } catch (e) {
+//     res.status(500).json({ error: e.message });
+//   }
+// });
+
+// app.get("/config", (req, res) => {
+//   res.send({
+//     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//   });
+// });
+
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { items } = req.body;
+
+//   // Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: "usd",
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
+
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
+app.use(express.static("public"));
+
+// const YOUR_DOMAIN = "http://localhost:5000";
+
+// app.post("/create-checkout-session", async (req, res) => {
+//   const session = await stripe.checkout.sessions.create({
+//     line_items: [
+//       {
+//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+//         price: req.body.product_ids,
+//         quantity: 1,
+//       },
+//     ],
+//     mode: "payment",
+//     success_url: `${YOUR_DOMAIN}?success=true`,
+//     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+//   });
+
+//   res.redirect(303, session.url);
+// });
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    console.log(" Inside /create-checkout-session");
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => {
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/failure",
+    });
+
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+export default app;
